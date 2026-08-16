abe695feddc6cf930ba780e2f62a250e36a5942b
taijylangu30-lgtm <taijylangu30@gmail.com>
2026-08-16 16:36:26 +0100

    Update connectMongoDB.js

--- database/connectDB/connectMongoDB.js
+++ database/connectDB/connectMongoDB.js
@@ -4,18 +4,15 @@ module.exports = async function (uriConnect) {
 	const userModel = require("../models/mongodb/user.js");
 	const dashBoardModel = require("../models/mongodb/userDashBoard.js");
 	const globalModel = require("../models/mongodb/global.js");
-	const bankModel = require("../models/mongodb/bank.js"); // Ajout du modèle bank
+	const bankModel = require("../models/mongodb/bank.js");
 
-	await mongoose.connect(uriConnect, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	});
+	await mongoose.connect(uriConnect);
 
 	return {
 		threadModel,
 		userModel,
 		dashBoardModel,
 		globalModel,
-		bankModel // Retour du modèle bank
+		bankModel
 	};
 };
