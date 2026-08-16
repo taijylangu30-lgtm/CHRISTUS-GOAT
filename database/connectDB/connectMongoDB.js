5d79a22bbc65c32b69bb97d7e25e57f68dbafc93
taijylangu30-lgtm <taijylangu30@gmail.com>
2026-08-16 15:16:47 +0100

    Include bank model in MongoDB connection
    Added bank model to the MongoDB connection module and updated mongoose connection options.

--- database/connectDB/connectMongoDB.js
+++ database/connectDB/connectMongoDB.js
@@ -1,18 +1,21 @@
 module.exports = async function (uriConnect) {
-        const mongoose = require("mongoose");
-        mongoose.set('strictQuery', false);
+	const mongoose = require("mongoose");
+	const threadModel = require("../models/mongodb/thread.js");
+	const userModel = require("../models/mongodb/user.js");
+	const dashBoardModel = require("../models/mongodb/userDashBoard.js");
+	const globalModel = require("../models/mongodb/global.js");
+	const bankModel = require("../models/mongodb/bank.js"); // Ajout du modèle bank
 
-        const threadModel = require("../models/mongodb/thread.js");
-        const userModel = require("../models/mongodb/user.js");
-        const dashBoardModel = require("../models/mongodb/userDashBoard.js");
-        const globalModel = require("../models/mongodb/global.js");
+	await mongoose.connect(uriConnect, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
 
-        await mongoose.connect(uriConnect);
-
-        return {
-                threadModel,
-                userModel,
-                dashBoardModel,
-                globalModel
-        };
+	return {
+		threadModel,
+		userModel,
+		dashBoardModel,
+		globalModel,
+		bankModel // Retour du modèle bank
+	};
 };
